8ba6a35d327f525227c6cba89c292d1e8ce1426f
mateonav98 <mateonav98@gmail.com>
2022-05-05 16:36:37 -0700

    adding get routes for specific ids

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,16 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/:id', async (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-//   try {
-//     const categoryData = await Category.findByPk(req.params.id, {
-//       include [{ model: Product, through: ProductTag}]
-//     });
-//     if  ()
-//   }
-// });
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
+    if (!categoryData) {
+      res.status(404).json({message: 'No category found with this ID'});
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 router.post('/', (req, res) => {
   // create a new category
